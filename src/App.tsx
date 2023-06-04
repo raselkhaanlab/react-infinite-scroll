6e89e0370324a289b3b882f63a036fe5c03a8d69
Md Rasel khan <raselkhanlab@gmail.com>
2023-06-04 15:33:59 +0600

    refactor: refactor code for data fetch and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { useFetchData } from './hooks/useFetchData';
@@ -8,47 +8,47 @@ import { useDebounce } from './hooks/useDebounce';
 function App() {
   const [query, setQuery] = useState("");
   const debounceQuery = useDebounce(query, 500);
-  const {isLoading, hasMore, loadMore, data} = useFetchData(debounceQuery || "");
+  const { isLoading, hasMore, loadMore, data } = useFetchData(debounceQuery || "");
   const [volumes, setVolumes] = useState(data);
-  
-  useEffect(()=>{
-    if(!debounceQuery) {
+
+  useEffect(() => {
+    if (!debounceQuery) {
       setVolumes([]);
     }
-  },[debounceQuery]);
+  }, [debounceQuery]);
 
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-      setVolumes([]);
-      setQuery(e.currentTarget.value);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVolumes([]);
+    setQuery(e.currentTarget.value);
   }
 
-  useEffect(()=>{
-    setVolumes((prev)=> Array.from(prev.concat(data)))
-  },[data]);
+  useEffect(() => {
+    setVolumes((prev) => Array.from(prev.concat(data)))
+  }, [data]);
 
   return (
     <>
       <p>React infinite scroll</p>
-      <input value={query} onChange={handleChange}/>
+      <input value={query} onChange={handleChange} />
       {query && <p> Search query: {query}</p>}
 
-       {isLoading && query &&  <p>Searching for : {query} ......</p>}
+      {isLoading && query && <p>Searching for : {query} ......</p>}
       <InfiniteScroll
         loadMoreFunc={loadMore.bind(null, query)}
-        hasMore={hasMore && volumes.length> 0}
+        hasMore={hasMore && volumes.length > 0}
         isLoading={isLoading}
-        >
-          <div style={{display:"flex", flexWrap: "wrap", justifyContent:"center",gap:"10px"}}>
+      >
+        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: "10px" }}>
           {
-              volumes.map((item,index)=>{
-                return <div 
-                key={item.id + index} 
-                style={{height: 200, width: 300, outline:"1px solid white", outlineOffset:"4px"}}>
-                  <img src={item?.volumeInfo?.imageLinks?.thumbnail} alt={item?.volumeInfo?.imageLinks?.thumbnail} style={{display: "block", height:"100%", width: "100%"}} />
-                </div>
-              })
+            volumes.map((item, index) => {
+              return <div
+                key={item.id + index}
+                style={{ height: 200, width: 300, outline: "1px solid green", margin: "2px", outlineOffset: "4px" }}>
+                <img src={item?.volumeInfo?.imageLinks?.thumbnail || item?.volumeInfo?.imageLinks?.smallThumbnail} alt={item?.volumeInfo?.imageLinks?.thumbnail || item?.volumeInfo?.imageLinks?.smallThumbnail} style={{ display: "block", height: "100%", width: "100%" }} />
+              </div>
+            })
           }
-          </div>
+        </div>
       </InfiniteScroll>
     </>
   )
--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,16 +1,16 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 interface IVolume {
-  id:string;
-  etag:string;
+  id: string;
+  etag: string;
   volumeInfo: {
-    title:string;
-    authors: string [];
+    title: string;
+    authors: string[];
     publisher: string;
     publishedDate: string;
     description: string;
     pageCount: number;
-    categories: string [];
+    categories: string[];
     language: string;
     previewLink: string;
     imageLinks: {
@@ -21,50 +21,51 @@ interface IVolume {
 }
 interface IVolumeResponse {
   totalItems: number;
-  items: IVolume []
+  items: IVolume[]
 }
 
 export function useFetchData(query: string) {
- const [data, setData] = useState <IVolume[]>([]);
- const [isLoading, setIsloading] = useState(false);
- const [hasMore, setHasMore] = useState(false);
- const [page, setPage] = useState(0);
- const abortController = useRef(new AbortController());
+  const [data, setData] = useState<IVolume[]>([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [hasMore, setHasMore] = useState(false);
+  const [page, setPage] = useState(0);
+  const abortController = useRef(new AbortController());
 
- const loadMore = async ()=> {
-  setIsloading(true);
-  setHasMore(false);
-  const maxResults= 10;
-  const startIndex = (page)*maxResults;
-  setPage(page + 1);
-  try {
-    const googleapi = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=10&startIndex=${startIndex}`;
-    const response = await fetch(googleapi, {signal: abortController.current.signal});
-    const responseData =await response.json() as unknown as IVolumeResponse;
-    const totalPage = Math.ceil(responseData.totalItems/maxResults) || 1;
-    if( page + 1 < totalPage ) {
-      setHasMore(true);
+  const loadMore = async () => {
+    setIsloading(true);
+    const maxResults = 10;
+    const startIndex = (page - 1) * maxResults;
+    const nextPage = page + 1;
+    setPage(nextPage);
+    try {
+      const googleapi = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=10&startIndex=${startIndex}`;
+      const response = await fetch(googleapi, { signal: abortController.current.signal });
+      const responseData = await response.json() as unknown as IVolumeResponse;
+      const totalPage = Math.ceil(responseData.totalItems / maxResults) || 1;
+      if (nextPage <= totalPage) {
+        setHasMore(true);
+      } else {
+        setHasMore(false);
+      }
+      setData(responseData && responseData.items || []);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsloading(false);
     }
-    setData(responseData && responseData.items || []);
-  } catch (e){
-    console.log(e);
-  } finally {
-    setIsloading(false);
-  }
-};
-
- useEffect(()=>{
-  if(query && page < 1 && data.length < 1) {
-    loadMore();
-  }
- },[page, data]);
+  };
 
 
+  useEffect(() => {
+    if (query && page == 1) {
+      loadMore();
+    }
+  }, [page, query]);
 
- useEffect(()=>{
-    setData([]);
-    setPage(0);
- },[query]);
 
- return {isLoading, hasMore, loadMore, data, abortController};
+  useEffect(() => {
+    setPage(1);
+  }, [query]);
+
+  return { isLoading, hasMore, loadMore, data, abortController };
 }
